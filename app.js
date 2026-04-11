5c5895cff5b86a1442f721e80c23e689b056cdf5
Gaurav Sananse <gauravsananse5@gmail.com>
2026-04-11 22:07:48 +0530

    .env condition changed

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
