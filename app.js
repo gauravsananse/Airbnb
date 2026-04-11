9621cca907038241dab9f136287bf37c3f440588
Gaurav Sananse <gauravsananse5@gmail.com>
2026-04-11 22:22:00 +0530

    new mongo link

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 
-const dbUrl = process.env.ATLASDB_URL;
+const dbUrl = "mongodb+srv://delta-student:TKJd9grtNvcZ0Q4f@cluster1.h3taqdb.mongodb.net/wanderlust?retryWrites=true&w=majority";
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
