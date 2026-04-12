4222e7fc2ce7375ba9ba49130e50a72a5e806213
Gaurav Sananse <gauravsananse5@gmail.com>
2026-04-12 13:02:30 +0530

    dns lines

--- app.js
+++ app.js
@@ -31,8 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 
-const dns = require("dns");
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 
 const dbUrl = "mongodb+srv://delta-student:TKJd9grtNvcZ0Q4f@cluster1.h3taqdb.mongodb.net/wanderlust?retryWrites=true&w=majority";
 
